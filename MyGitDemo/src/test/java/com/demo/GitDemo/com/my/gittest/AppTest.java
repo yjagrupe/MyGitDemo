d5920f60d4f68bc6ed56a774da8bdf6662274bc3
Sai <Sai@Sai-LAP>
2018-09-08 22:07:10 +0530

    msg added

--- MyGitDemo/src/test/java/com/demo/GitDemo/com/my/gittest/AppTest.java
+++ MyGitDemo/src/test/java/com/demo/GitDemo/com/my/gittest/AppTest.java
@@ -16,5 +16,6 @@ public class AppTest
         System.out.println( "Hello Git msg 2!!!!" );
         System.out.println( "Hello Git msg 3!!!!" );
         System.out.println( "Hello Git msg 4!!!!" );
+        System.out.println( "Hello Git msg 5!!!!" );
     }
 }
